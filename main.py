83d6084a7eceec4a4f06703ca0fca8425852284b
Aliaksei Tarasenka <aliaksei.tarasenka@aegeurope.com>
2024-04-29 13:43:41 +0200

    Implement Singleton pattern to the Main window

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from mainwindow import *
 
-if __name__ == '__main__':
+
+def main():
     handle = Window()
+    handle2 = Window()
 
     handle.title('Daw')
     handle.geometry('1280x720')
 
+    assert (handle2 is handle)
+    handle2.title('Daw')
+    handle2.geometry('1280x720')
+
     handle.mainloop()
+    handle2.mainloop()
+
+
+if __name__ == '__main__':
+    main()
--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,11 @@
 from tkinter import *
+from creational.singleton import Singleton
 
 
-class Window(Tk):
-    def __init__(self):
+class Window(Tk, Singleton):
+    def init(self):
+        print('calling from __init__ for the instance')
         super().__init__()
+
+    def __init__(self):
+        print('calling from __init__ class constructor')
